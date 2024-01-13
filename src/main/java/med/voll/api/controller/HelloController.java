cc7fb35d1c3074d3cb5045217117961e29e8d946
Neto <jfredericon@gmail.com>
2024-01-13 12:02:42 -0300

    Olá mundo!

---
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -0,0 +1,17 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World Spring";
+    }
+    
+}
