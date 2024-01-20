dc2c2ed5a1630f598aa6f688fcde1a1cafb13a32
Neto <jfredericon@gmail.com>
2024-01-20 09:14:26 -0300

    Adicionado o SpringDoc ao projeto

--- src/main/java/med/voll/api/controller/ConsultationController.java
+++ src/main/java/med/voll/api/controller/ConsultationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consultation.ConsultationCancelData;
@@ -17,6 +18,7 @@ import med.voll.api.domain.consultation.ConsultationScheduleData;
 
 @RestController
 @RequestMapping("consultations")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultationController {
 
     @Autowired
